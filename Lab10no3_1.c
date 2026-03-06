bab9cf5ac87405a8681a9ecc35042192e5ae58a1
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2026-03-06 11:56:44 +0700

    Update Lab10no3_1.c

--- Lab10no3_1.c
+++ Lab10no3_1.c
@@ -19,3 +19,5 @@ printf("Book ID : %d \n",CBook.BookID);
 printf("Book Title : %s \n",CBook.BookTitle); 
 } 
 
+//void DisplayData(struct Books CBook) คือ ฟังก์ชันเเสดงผลที่เราสร้างรับ ค่าของโครงสร้าง (Struct) เข้ามาเพื่อแสดงผล
+//พารามิเตอร์ที่รับเข้ามา รับobjectเข้ามาในฟังก์ชัน รับแบบ "Pass by Value"
